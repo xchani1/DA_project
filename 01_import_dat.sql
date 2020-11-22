8524e23c230b5d7cc14e09a308baa1c3218a1310
xchani1 <74879424+xchani1@users.noreply.github.com>
2020-11-22 22:27:38 +0100

    Add files via upload

---
+++ 01_import_dat.sql
@@ -0,0 +1,12 @@
+use hrdata
+truncate table t_import
+
+BULK INSERT t_import FROM 'C:\Users\alena.novotna\Desktop\soukr\DA_2020\Projekt\_data\unicode\102020_EU.csv'
+WITH (
+DATAFILETYPE = 'widechar',
+FIELDTERMINATOR = ';',
+ROWTERMINATOR = '\n',
+FIRSTROW = 2
+)
+
+update t_import SET ID=convert(int, ID)+1000000 WHERE Mandant='MAND30'
